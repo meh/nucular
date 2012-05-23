eae4a774a83e69cf809b5626c29c823f3aac80d9
meh <meh@paranoici.org>
2012-05-24 00:38:59 +0200

    Improve the proxy example.

--- examples/proxy.d
+++ examples/proxy.d
@@ -17,6 +17,8 @@
  ****************************************************************************/
 
 import std.stdio;
+import std.regex;
+
 import nucular.reactor;
 import nucular.protocols.socks;
 
@@ -24,18 +26,27 @@ class Reader : Connection
 {
 	override void connected ()
 	{
-		sendData(cast (ubyte[]) "GET / HTTP/1.1\r\nConnection:close\r\nHost: google.com\r\n\r\n");
+		sendData(cast (ubyte[]) "GET /n09230945.asp HTTP/1.1\r\nConnection: close\r\nHost: automation.whatismyip.com\r\n\r\n");
 	}
 
 	override void receiveData (ubyte[] data)
 	{
-		writeln(cast (string) data);
+		if (auto m = (cast (string) data).match(r"(\d+\.\d+\.\d+\.\d+)")) {
+			writeln(m.captures[1]);
+
+			closeConnection();
+		}
+	}
+
+	override void unbind ()
+	{
+		nucular.reactor.stop();
 	}
 }
 
 void main ()
 {
 	nucular.reactor.run({
-		(new ProxiedAddress("google.com", 80)).connectThrough!Reader(new InternetAddress("localhost", 9050));
+		(new ProxiedAddress("automation.whatismyip.com", 80)).connectThrough!Reader(new InternetAddress("localhost", 9050));
 	});
 }
